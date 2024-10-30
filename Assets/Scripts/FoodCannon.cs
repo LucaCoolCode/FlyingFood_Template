b4cce173bd76d5a754440c9c9d03928547ef7a47
Daniel <logitin@pm.me>
2024-10-30 17:02:56 +0100

    Removed scripts and functionalities

--- Assets/Scripts/FoodCannon.cs
+++ Assets/Scripts/FoodCannon.cs
@@ -2,21 +2,5 @@ using UnityEngine;
 
 public class FoodCannon : MonoBehaviour
 {
-    [SerializeField] private float foodSpeed = 10;
-    [SerializeField] private Transform shootPoint;
-    [SerializeField] private GameObject[] food;
 
-    private void Shoot()
-    {
-        GameObject newFood = Instantiate(food[0], shootPoint.position, Random.rotation);
-        newFood.GetComponent<Rigidbody>().velocity = shootPoint.forward * foodSpeed;
-    }
-
-    private void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Shoot();
-        }
-    }
 }
