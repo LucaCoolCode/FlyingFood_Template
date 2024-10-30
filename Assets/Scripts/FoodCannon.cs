e377658574dd1225dc83fd527c750600ee3b7ba4
LucaCoolCode <luca@coolcode.io>
2024-10-30 18:30:47 +0100

    Toast atacke
    yayyyyyyy

--- Assets/Scripts/FoodCannon.cs
+++ Assets/Scripts/FoodCannon.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class FoodCannon : MonoBehaviour
 {
+    [SerializeField] private float foodSpeed = 30;
+    [SerializeField] private GameObject food;
+    [SerializeField] private Transform ShootPoint;
 
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+
+            Shoot();
+
+
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(food, ShootPoint.position, Random.rotation);
+        
+    }
 }
