4d34aa85853dcd3a4032b54eb688850e00847552
LucaCoolCode <luca@coolcode.io>
2024-11-20 18:30:50 +0100

    wham effect plate respawn

--- Assets/Scripts/Gegner.cs
+++ Assets/Scripts/Gegner.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class Gegner : MonoBehaviour
 {
+    [SerializeField] private Transform spawnPointHolder;
+
+    [SerializeField] private GameObject hitEffect;
+    [SerializeField] private GameObject appearEffect;
+    private List<Transform> allSpawns = new List<Transform>();
+
+    private void Awake()
+    {
+        foreach (Transform spawnPoint in spawnPointHolder)
+        {
+            allSpawns.Add(spawnPoint);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        print("hit");
+        Respawn();
+    }
+
+    private void Respawn()
+    {
+        Instantiate(hitEffect, transform.position, transform.rotation);
+        Transform randomSpawn = allSpawns[Random.Range(0, allSpawns.Count)];
+        transform.SetPositionAndRotation(randomSpawn.position, randomSpawn.rotation);
+        Instantiate(appearEffect, transform.position, transform.rotation);
+
+
     }
 }
