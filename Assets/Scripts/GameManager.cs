87720ce58c7f85b664bb0a7f33cff17a0b62639c
LucaCoolCode <luca@coolcode.io>
2025-03-12 15:00:17 +0100

    timer begonnen
    if 0<= Scenemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private int timer = 12;
+
+    private void CountDown()
+    {
+        timer--;
+        print(timer);
+
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating(nameof(CountDown),1,1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
