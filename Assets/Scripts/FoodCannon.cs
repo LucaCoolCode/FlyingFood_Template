e5e6d500e9d6a606847e16271e559c3e86bd64ce
LucaCoolCode <luca@coolcode.io>
2025-03-12 14:11:16 +0100

    Merge Konflikt lösen

--- Assets/Scripts/FoodCannon.cs
+++ Assets/Scripts/FoodCannon.cs
@@ -53,3 +53,33 @@ public class FoodCannon : MonoBehaviour
     }
 }
 >>>>>>> Stashed changes
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodCannon : MonoBehaviour
+{
+    [SerializeField] private float foodSpeed = 30;
+    [SerializeField] private Transform ShootPoint;
+    [SerializeField] private GameObject ShootEffect;
+    [SerializeField] private List<GameObject> FoodList;
+     
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+
+            Shoot();
+
+
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject randomFood = FoodList[Random.Range(0, FoodList.Count)];
+        GameObject newFood = Instantiate(randomFood, ShootPoint.position, Random.rotation);
+        newFood.GetComponent<Rigidbody>().velocity = ShootPoint.forward * foodSpeed;
+        Instantiate(ShootEffect, ShootPoint.position, Quaternion.identity);
+    }
+}
