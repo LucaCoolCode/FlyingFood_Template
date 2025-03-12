e58be839b362c250d49280e47e0a6510f2912af1
LucaCoolCode <luca@coolcode.io>
2025-03-12 14:13:54 +0100

    Merge Konflikt lösen

--- Assets/Scripts/FoodCannon.cs
+++ Assets/Scripts/FoodCannon.cs
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-using UnityEngine;
-
-public class FoodCannon : MonoBehaviour
-{
-    [SerializeField] private float shootSpeed = 30;
-    [SerializeField] private Transform shootPoint;
-    [SerializeField] private GameObject food;
-
-    private void Shoot()
-    {
-        GameObject newFood = Instantiate(food, shootPoint.position, Random.rotation);
-        newFood.GetComponent<Rigidbody>().velocity = shootPoint.forward * shootSpeed;
-    }
-
-    private void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Shoot();
-        }
-    }
-}
-=======
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -36,42 +12,7 @@ public class FoodCannon : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-
-
             Shoot();
-
-
-        }
-    }
-
-    private void Shoot()
-    {
-        GameObject randomFood = FoodList[Random.Range(0, FoodList.Count)];
-        GameObject newFood = Instantiate(randomFood, ShootPoint.position, Random.rotation);
-        newFood.GetComponent<Rigidbody>().velocity = ShootPoint.forward * foodSpeed;
-        Instantiate(ShootEffect, ShootPoint.position, Quaternion.identity);
-    }
-}
->>>>>>> Stashed changes
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FoodCannon : MonoBehaviour
-{
-    [SerializeField] private float foodSpeed = 30;
-    [SerializeField] private Transform ShootPoint;
-    [SerializeField] private GameObject ShootEffect;
-    [SerializeField] private List<GameObject> FoodList;
-     
-    private void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-
-
-            Shoot();
-
-
         }
     }
 
