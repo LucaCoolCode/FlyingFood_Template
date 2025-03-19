0c3e4e13000ac8ab2c13160c0a1392fa74a2efb7
LucaCoolCode <luca@coolcode.io>
2025-03-19 14:28:46 +0100

    ethan timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using TMPro;
+using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    private int timer = 12;
+    [SerializeField] private TMP_Text timerText;
+    private int timer = 30;
 
     private void CountDown()
     {
         timer--;
-        print(timer);
+        timerText.text = timer.ToString();
 
         if (timer <= 0)
         {
@@ -19,13 +22,15 @@ public class GameManager : MonoBehaviour
         }
     }
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        timerText.text = timer.ToString();
+
         InvokeRepeating(nameof(CountDown),1,1);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
     }
 }
