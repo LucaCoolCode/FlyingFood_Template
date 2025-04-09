96d42026d42bf7479b5b6412a08d81a67dea29a6
LucaCoolCode <luca@coolcode.io>
2025-04-09 14:12:12 +0200

    pfeil zeigt auf ziel

--- Assets/Scripts/TargetArrow.cs
+++ Assets/Scripts/TargetArrow.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Target : MonoBehaviour
+public class TargetArrow : MonoBehaviour
 {
+    [SerializeField] private Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(target);
     }
 }
