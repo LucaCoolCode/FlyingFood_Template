02452941532f4d2bc7481012179e1f5c41e728d1
LucaCoolCode <luca@coolcode.io>
2025-04-09 14:28:16 +0200

    full game with unternehmensreexchtschutzversicherung

---
+++ Assets/Scripts/AveragePizzaEnjoyer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AveragePizzaEnjoyer: MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Unternehmensrechtschutzversicherung");
+    }
+}
